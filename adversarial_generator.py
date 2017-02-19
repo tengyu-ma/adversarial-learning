418e73eabed0bea8924a18c83f032ee59151a7a3
Huahong Zhang <huahong.zhang@vanderbilt.edu>
2017-02-18 21:34:57 -0600

    fixed the bug of GPU when the size is too large

--- adversarial_generator.py
+++ adversarial_generator.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import numpy as np
 
 def fast_gradient_sign_method(J, x, y_, x_test, y_test, sess, keep_prob, drop_out=False, epsilon=0.1):
     """
@@ -19,12 +19,22 @@ def fast_gradient_sign_method(J, x, y_, x_test, y_test, sess, keep_prob, drop_ou
     nabla_J = tf.gradients(J, x)  # apply nabla operator to calculate the gradient
     sign_nabla_J = tf.sign(nabla_J)  # calculate the sign of the gradient of cost function
     eta = tf.multiply(sign_nabla_J, epsilon)  # multiply epsilon the sign of the gradient of cost function
-    if drop_out:
-        eta_result = sess.run(eta, feed_dict={x: x_test, y_: y_test, keep_prob: 1.0})
-    else:
-        eta_result = sess.run(eta, feed_dict={x: x_test, y_: y_test})
+    x_test_adversarial = []
 
-    eta_flatten = eta_result[0]  # since we only have 1 row, we need to flat the eta. e.g. [[[a1,a2,..,an]]] to [[a1,a2,..,an]]
+    test_size = x_test.size // 784
+    for i in range(int(test_size // 1000)):
+        x_temp = x_test[i * 1000:(i + 1) * 1000]
+        y_temp = y_test[i * 1000:(i + 1) * 1000]
+        if drop_out:
+            eta_result = sess.run(eta, feed_dict={x: x_temp, y_: y_temp, keep_prob: 1.0})
+        else:
+            eta_result = sess.run(eta, feed_dict={x: x_temp, y_: y_temp})
 
-    x_test_adversarial = sess.run(tf.add(x_test, eta_flatten))  # add noise to test data
+        eta_flatten = eta_result[0]  # since we only have 1 row, we need to flat the eta. e.g. [[[a1,a2,..,an]]] to [[a1,a2,..,an]]
+        temp = sess.run(tf.add(x_temp, eta_flatten))  # add noise to test data
+        if x_test_adversarial == []:
+            x_test_adversarial = temp
+        else:
+            x_test_adversarial = np.vstack((x_test_adversarial,temp))
+
     return x_test_adversarial
