440844dd8eebbd0d36d23049b322825e34bc67ff
HuahongZhang <hauhong.zhang@vanderbilt.edu>
2017-05-14 18:34:46 +0800

    cleaning the code

--- models/cifar10/settings.py
+++ models/cifar10/settings.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 
+# Global constants describing the CIFAR-10 data set.
+NUM_CLASSES = 10
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/cifar10_train',
                            """Directory where to read model checkpoints.""")
@@ -26,20 +31,11 @@ tf.app.flags.DEFINE_boolean('run_once', True, """Whether to run eval only once."
 tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
                            """Directory where to write event logs and checkpoint.""")
 tf.app.flags.DEFINE_boolean('use_fp16', False, """Train the model using fp16.""")
+tf.app.flags.DEFINE_integer('batch_size', 128, """Number of images to process in a batch.""")
 
-# Global constants describing the CIFAR-10 data set.
-NUM_CLASSES = 10
-NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
-NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
-
-IS_EVAL_DATA = 'test'  # 'train_eval'
-# EVAL_DIR = '/tmp/cifar10_eval'
-# DATA_DIR = '/tmp/cifar10_data/cifar-10-batches-bin'
-# CHECKPOINT_DIR = '/tmp/cifar10_train'
-
-# MODE = '32_to_24'
+MODE = '32_to_24'
 # MODE = '24_to_noise'
-MODE = 'normal'
+# MODE = 'normal'
 # MODE = 'show'
 # MODE = 'vote_show'
 # MODE = 'train'
@@ -56,8 +52,8 @@ else:
     EVAL_DATA = 'test_batch_org.bin'
     # ORG_IMAGE_SIZE = 24
 
-if MODE == 'normal' or MODE == 'train':
-    tf.app.flags.DEFINE_integer('batch_size', 128, """Number of images to process in a batch.""")
-else:
-    tf.app.flags.DEFINE_integer('batch_size', 1, """Number of images to process in a batch.""")
+# if MODE == 'normal' or MODE == 'train':
+#     tf.app.flags.DEFINE_integer('batch_size', 128, """Number of images to process in a batch.""")
+# else:
+#     tf.app.flags.DEFINE_integer('batch_size', 1, """Number of images to process in a batch.""")
 # tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data', """Path to the CIFAR-10 data directory.""")
