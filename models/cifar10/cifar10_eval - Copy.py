82022ac13ea6a42ce1ecf99b9200a24091620f7b
HuahongZhang <hauhong.zhang@vanderbilt.edu>
2017-05-15 19:45:49 +0800

    trying to get accuracy 0.86

--- models/cifar10/cifar10_eval - Copy.py
+++ models/cifar10/cifar10_eval - Copy.py
@@ -40,28 +40,14 @@ import time
 
 import numpy as np
 import tensorflow as tf
+from settings import *
 
 import cifar10
-
-FLAGS = tf.app.flags.FLAGS
-
-tf.app.flags.DEFINE_string('eval_dir', '/tmp/cifar10_eval',
-                           """Directory where to write event logs.""")
-tf.app.flags.DEFINE_string('eval_data', 'test',
-                           """Either 'test' or 'train_eval'.""")
-tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/cifar10_train',
-                           """Directory where to read model checkpoints.""")
-tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
-                            """How often to run the eval.""")
-tf.app.flags.DEFINE_integer('num_examples', 10000,
-                            """Number of examples to run.""")
-tf.app.flags.DEFINE_boolean('run_once', False,
-                            """Whether to run eval only once.""")
 
 
 def eval_once(saver, summary_writer, top_k_op, summary_op):
     """Run Eval once.
-  
+
     Args:
       saver: Saver.
       summary_writer: Summary writer.
@@ -115,11 +101,11 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
 
 def evaluate():
     """Eval CIFAR-10 for a number of steps."""
-    with tf.Graph().as_default() as g:
+    with tf.device('/cpu:0'):
         with tf.variable_scope('network1') as scope:
             # Get images and labels for CIFAR-10.
             eval_data = FLAGS.eval_data == 'test'
-            images, labels = cifar10.inputs(eval_data=eval_data)
+            images, labels, org_images = cifar10.inputs(eval_data=eval_data)
 
             # Build a Graph that computes the logits predictions from the
             # inference model.
@@ -137,7 +123,7 @@ def evaluate():
             # Build the summary operation based on the TF collection of Summaries.
             summary_op = tf.summary.merge_all()
 
-            summary_writer = tf.summary.FileWriter(FLAGS.eval_dir, g)
+            summary_writer = tf.summary.FileWriter(FLAGS.eval_dir)
 
             while True:
                 eval_once(saver, summary_writer, top_k_op, summary_op)
